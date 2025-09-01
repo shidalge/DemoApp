fad42579aa61ee518eca267d33043e5a9c121bca
SERGIO HERNAN HIDALGO ESPINOZA <sehhidal@pichincha.com>
2025-09-01 12:29:49 -0500

    Some refactor

--- DemoApp/Controllers/WeatherForecastController.cs
+++ DemoApp/Controllers/WeatherForecastController.cs
@@ -5,32 +5,24 @@ namespace DemoApp.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController() : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries =
+        [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        ];
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             Log.Information("Weather info returned");
 
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            })];
         }
 
         [HttpGet("force500")]
